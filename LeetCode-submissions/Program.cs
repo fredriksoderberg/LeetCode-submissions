5284a85fc69d41920cd3274615901e4e38d6d871
y510p <frsoder@gmail.com>
2016-02-09 16:11:46 +0100

    .

---
+++ LeetCode-submissions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_submissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Out.Write(Submissions.CanWinNim(4));
+            Console.In.ReadLine();
+        }
+    }
+}
---
+++ LeetCode-submissions/Submissions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_submissions
+{
+    static class Submissions
+    {
+        static public bool CanWinNim(int i)
+        {
+            if (i == 0)
+                return false;
+            else if (i >= 1 || i <= 3)
+                return true;
+            else
+                return (CanWinNim(i - 3));
+
+        }
+
+    }
+}
