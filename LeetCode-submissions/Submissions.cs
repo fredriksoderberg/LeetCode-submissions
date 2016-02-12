92c1c78a2bef62bad246393223420326ea6755f5
y510p <frsoder@gmail.com>
2016-02-12 22:27:40 +0100

    .

--- LeetCode-submissions/Submissions.cs
+++ LeetCode-submissions/Submissions.cs
@@ -13,7 +13,7 @@ namespace LeetCode_submissions
         {
             if (n % 4 == 0)
                 return false;
-            else 
+            else
                 return true;
         }
 
@@ -43,13 +43,77 @@ namespace LeetCode_submissions
                 return System.Math.Max(MaxDepth(root.right) + 1, MaxDepth(root.left) + 1);
         }
 
+        static public void DeleteNode(ListNode node)
+        {
+            if (node.next == null)
+                return;
+            else
+            {
+                node.val = node.next.val;
+                node.next = node.next.next;
+            }
+        }
+
+        static public TreeNode InvertTree(TreeNode root)
+        {
+            InvertTreeFromTopNode(root);
+            return root;
+        }
+
+        static public void InvertTreeFromTopNode(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            InvertTreeFromTopNode(root.left);
+            InvertTreeFromTopNode(root.right);
+        }
+
+        static public void MoveZeroes(int[] nums)
+        {
+
+            int numberOfZeros = 0;
+
+            for (int i = 0, j = 0; j < nums.Length; i++, j++)
+            {
+                if (nums[i] == 0)
+                {
+                    j++;
+                    numberOfZeros++;
+                }
+
+                nums[i] = nums[j];
+
+            }
+
+            for(int i = 1; i <= numberOfZeros; i++)
+            {
+                nums[nums.Length - i] = 0;
+            }
+        }
+
+
+
+
     }
 
     public class TreeNode
     {
-         public int val;
-         public TreeNode left;
-         public TreeNode right;
-         public TreeNode(int x) { val = x; }
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+
     }
 }
